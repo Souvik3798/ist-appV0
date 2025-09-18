ffa425e8a6bb5ddb334e9d71ec3ab10ed02f1194
Dinesh Lal <40160990+dineshlalgit@users.noreply.github.com>
2025-09-18 14:33:58 +0530

    update blogs/ config

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+    basePath: '/blogs',        // 👈 ensures routes work from /blogs
+    assetPrefix: '/blogs/',    // 👈 ensures CSS/JS assets load from /blogs
+    trailingSlash: true,
 }
 
 export default nextConfig
