67fc6c8d58269d869c126092afcafe28fa3d6478
Souvik Kumar Barua <69067076+Souvik3798@users.noreply.github.com>
2025-09-18 15:09:53 +0530

    updated previous changes

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,7 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-  },
-    basePath: '/blogs',        // 👈 ensures routes work from /blogs
-    assetPrefix: '/blogs/',    // 👈 ensures CSS/JS assets load from /blogs
-    trailingSlash: true,
+  }
 }
 
 export default nextConfig
